854837863766bf5d4b85f105ef73198820aa8d18
Torba Igor <garrytrue@yandex.ru>
2016-06-01 11:06:36 +0300

    Add models

---
+++ app/src/main/java/com/tiv/ocpapp/model/Question.java
@@ -0,0 +1,52 @@
+package com.tiv.ocpapp.model;
+
+import java.util.List;
+
+
+public class Question {
+    private int id;
+    private String text;
+    private List<Answer> answers;
+    private String description;
+    private List<Integer> correctAnswersIds;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Integer> getCorrectAnswersIds() {
+        return correctAnswersIds;
+    }
+
+    public void setCorrectAnswersIds(List<Integer> correctAnswersIds) {
+        this.correctAnswersIds = correctAnswersIds;
+    }
+}
