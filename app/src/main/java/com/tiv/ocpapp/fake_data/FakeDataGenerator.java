f474cd51fc93100d8dd0865e540184a5d2bfd821
Torba Igor <garrytrue@yandex.ru>
2016-06-01 13:51:47 +0300

    Fix activity orientation in manifest

---
+++ app/src/main/java/com/tiv/ocpapp/fake_data/FakeDataGenerator.java
@@ -0,0 +1,7 @@
+package com.tiv.ocpapp.fake_data;
+
+/**
+ * Created by tiv on 01.06.2016.
+ */
+public class FakeDataGenerator {
+}
---
+++ app/src/main/java/com/tiv/ocpapp/mvp/MainView.java
@@ -0,0 +1,7 @@
+package com.tiv.ocpapp.mvp;
+
+/**
+ * Created by tiv on 01.06.2016.
+ */
+public interface MainView {
+}
